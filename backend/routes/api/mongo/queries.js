d7656cd90bc13fcf01ad91499164da9c5194b1fb
KIKO <fjmo_1997@hotmail.com>
2020-04-02 21:25:59 +0100

    backend - improving backend error response PART2
    -all mongo error responses are now improved to give the best possible error status and message for the known errors
    -this will be used to improve frontend error messages to the user
    -part3 will be the updated swagger doc

--- backend/routes/api/mongo/queries.js
+++ backend/routes/api/mongo/queries.js
@@ -5,53 +5,111 @@ var Queries = require("../../../controllers/query")
 router.get('/', function (req, res) {
   Queries.list()
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.get('/user/:email', function (req, res) {
   Queries.listByUserEmail(req.params.email)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.get('/user/:email/global', function (req, res) {
   Queries.listByUserEmailGlobal(req.params.email)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.get('/user/:email/:repo', function (req, res) {
   Queries.listByUserEmailRepo(req.params.email, req.params.repo)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.get('/:name', function (req, res) {
   Queries.getQuery(req.params.name)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.post('/', function (req, res) {
   // console.dir(req.body)
-  console.log(">log-router API/QUERIES inserting into database: " + JSON.stringify(req.body))
+  // console.log(">log-router API/QUERIES inserting into database: " + JSON.stringify(req.body))
   Queries.createQuery(req.body)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.put('/:user/:name', function (req, res) {
   // console.dir(req.body)
-  console.log(">log-router API/USERS altering query: " + JSON.stringify(req.body))
+  // console.log(">log-router API/USERS altering query: " + JSON.stringify(req.body))
   Queries.updateQueryValue(req.params.user, req.params.name, req.body)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.delete('/:user/:name', function (req, res) {
   Queries.deleteQuery(req.params.user, req.params.name)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 })
+
+//// Auxiliar function ////
+// process errors and return a more standard error response
+function processError(error) {
+  var status = 500
+  var data = error // "MONGO unknown error :("
+  // Duplicate Keys (Duplicate Query name and UserID)
+  if (error.name === 'MongoError' && error.code === 11000) {
+    status = 409
+    data = error.errmsg // FIXME: prepare a more readable response
+  }
+  // Bad Request (missing required variable, bad syntax, etc)
+  else if (error.name === 'ValidationError') {
+    status = 400
+    data = error.message
+  }
+  return {status: status, data: data}
+}
 
 module.exports = router;
--- backend/routes/api/mongo/users.js
+++ backend/routes/api/mongo/users.js
@@ -5,33 +5,76 @@ var Users = require("../../../controllers/user")
 router.get('/', function (req, res) {
   Users.list()
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.get('/:email', function (req, res) {
   Users.getUser(req.params.email)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.post('/', function (req, res) {
   // console.log(">> API/USERS inserting user: " + JSON.stringify(req.body))
   Users.createUser(req.body)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.put('/:email/password', function (req, res) {
   // console.log(">> API/USERS altering password: " + JSON.stringify(req.body))
   Users.updateUserPassword(req.params.email, req.body)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 });
 
 router.delete('/:email', function (req, res) {
   Users.deleteUser(req.params.email)
     .then(data => res.jsonp(data))
-    .catch(error => res.status(500).jsonp(error))
+    .catch(error => {
+      try {
+        var errorAux = processError(error)
+        res.status(errorAux.status).send(errorAux.data) }
+      catch { res.status(500).send("MONGO unknown error :(") }
+    });
 })
+
+//// Auxiliar function ////
+// process errors and return a more standard error response
+function processError(error) {
+  var status = 500
+  var data = error // "MONGO unknown error :("
+  // Duplicate Keys (Duplicate Query name and UserID)
+  if (error.name === 'MongoError' && error.code === 11000) {
+    status = 409
+    data = error.errmsg // FIXME: prepare a more readable response
+  }
+  // Bad Request (missing required variable, bad syntax, etc)
+  else if (error.name === 'ValidationError') {
+    status = 400
+    data = error.message
+  }
+  return {status: status, data: data}
+}
 
 module.exports = router;
