ff26c0eb382b63039b40781ad363e24ee8a4b305
KIKO <fjmo_1997@hotmail.com>
2020-04-03 21:05:07 +0100

    backend - sparql queries improved
    -default routes improved to accept any query type, but the user must be careful to use the correct accept and content-type as needed
    -added a special route for SELECT, CONSTRUCT and ASK queries
    +this route exist to better understand what each query type accepts and returns. but they should work as normal

--- backend/routes/api/rdf4j/query.js
+++ backend/routes/api/rdf4j/query.js
@@ -9,15 +9,14 @@ var rdf4j = urls.rdf4j
 var rdf4jServer = 'http://'+rdf4j+'/rdf4j-server/'
 
 
-//// Queries ////
-// query (GET) (params in queryParams)
+//// Any Query ////
+//// Accept anything, returns something
+// SPARQL 1.1 Query (GET) (params in queryParams)
+// Caution advised when choosing "Accept" value as it changes depending on Query type
 router.get('/:repo', function (req, res) {
   const repo = req.params.repo
   const params = req.query
-  // const accept = "text/csv" // HARDCODE text/csv (rdf4j default)
-  // const accept = "application/json" // HARDCODE application/json
-  var accept = req.headers['accept'] // use req accept value
-  if (accept === "*/*") accept = 'application/json' // if no accept is given use JSON
+  const accept = req.headers['accept']
   const url = rdf4jServer + 'repositories/' + repo
   const config = {
     params: params,
@@ -26,12 +25,31 @@ router.get('/:repo', function (req, res) {
     }
   }
   axios.get(url, config)
-    .then(response => {
-      if (accept === "application/json")
-        res.jsonp(response.data)
-      else
-        res.send(response.data)
-    })
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+
+// SPARQL 1.1 Query (POST) (params in x-www-form-urlencoded)
+// Caution advised when choosing a "Accept" and "Content-Type" as they change depending on Query type
+router.post('/:repo', function (req, res) {
+  const repo = req.params.repo
+  const accept = req.headers['accept']
+  const url = rdf4jServer + 'repositories/' + repo
+  const body = qs.stringify(req.body)
+  const config = {
+    headers: {
+      'Accept': accept,
+      "Content-Type": 'application/x-www-form-urlencoded',
+    }
+  }
+  axios.post(url, body, config)
+    .then(response => res.send(response.data))
     .catch(error => {
       try { res.status(error.response.status).send(error.response.data) }
       catch {
@@ -42,12 +60,37 @@ router.get('/:repo', function (req, res) {
 });
 
 
-// query (POST) (params in x-www-form-urlencoded)
-// query (POST) (accepts text/csv)
-router.post('/:repo', function (req, res) {
+
+//// Specific Query ////
+//// Expects a certain type of Query type
+// Select (GET) (Table Result)
+router.get('/select/:repo', function (req, res) {
+  const repo = req.params.repo
+  const params = req.query
+  // const accept = "text/csv" // HARDCODE text/csv (rdf4j default)
+  var accept = req.headers['accept'] // use req accept value
+  if (accept === "*/*") accept = 'application/json' // if no accept is given use JSON
+  const url = rdf4jServer + 'repositories/' + repo
+  const config = {
+    params: params,
+    headers: {
+      'Accept': accept,
+    }
+  }
+  axios.get(url, config)
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+// Select (POST) (Table Result)
+router.post('/select/:repo', function (req, res) {
   const repo = req.params.repo
   // const accept = "text/csv" // HARDCODE text/csv (rdf4j default)
-  // const accept = "application/json" // HARDCODE application/json
   var accept = req.headers['accept'] // use req accept value
   if (accept === "*/*") accept = 'application/json' // if no accept is given use JSON
   const url = rdf4jServer + 'repositories/' + repo
@@ -59,12 +102,7 @@ router.post('/:repo', function (req, res) {
     }
   }
   axios.post(url, body, config)
-    .then(response => {
-      if(accept==="application/json")
-        res.jsonp(response.data)
-      else
-        res.send(response.data)
-    })
+    .then(response => res.send(response.data))
     .catch(error => {
       try { res.status(error.response.status).send(error.response.data) }
       catch {
@@ -73,5 +111,194 @@ router.post('/:repo', function (req, res) {
       }
     });
 });
+
+// Construct (GET) (RDF/Triples Result)
+router.get('/construct/:repo', function (req, res) {
+  const repo = req.params.repo
+  const params = req.query
+  // const accept = "text/plain" // HARDCODE text/plain (rdf4j default)
+  var accept = req.headers['accept'] // use req accept value
+  if (accept === "*/*") accept = 'application/rdf+json' // if no accept is given use JSON
+  const url = rdf4jServer + 'repositories/' + repo
+  const config = {
+    params: params,
+    headers: {
+      'Accept': accept,
+    }
+  }
+  axios.get(url, config)
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+// Construct (POST) (RDF/Triples Result)
+router.post('/construct/:repo', function (req, res) {
+  const repo = req.params.repo
+  // const accept = "text/plain" // HARDCODE text/plain (rdf4j default)
+  var accept = req.headers['accept'] // use req accept value
+  if (accept === "*/*") accept = 'application/rdf+json' // if no accept is given use JSON
+  const url = rdf4jServer + 'repositories/' + repo
+  const body = qs.stringify(req.body)
+  const config = {
+    headers: {
+      'Accept': accept,
+      "Content-Type": 'application/x-www-form-urlencoded',
+    }
+  }
+  axios.post(url, body, config)
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+
+// Ask (GET) (Boolean Result)
+router.get('/ask/:repo', function (req, res) {
+  const repo = req.params.repo
+  const params = req.query
+  // const accept = "application/sparql-results+json" // HARDCODE application/sparql-results+json (rdf4j default)
+  var accept = req.headers['accept'] // use req accept value
+  if (accept === "*/*") accept = 'text/boolean' // if no accept is given use text/boolean
+  const url = rdf4jServer + 'repositories/' + repo
+  const config = {
+    params: params,
+    headers: {
+      'Accept': accept,
+    }
+  }
+  axios.get(url, config)
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+// Ask (POST) (Boolean Result)
+router.post('/ask/:repo', function (req, res) {
+  const repo = req.params.repo
+  // const accept = "application/sparql-results+json" // HARDCODE application/sparql-results+json (rdf4j default)
+  var accept = req.headers['accept'] // use req accept value
+  if (accept === "*/*") accept = 'text/boolean' // if no accept is given use text/boolean
+  const url = rdf4jServer + 'repositories/' + repo
+  const body = qs.stringify(req.body)
+  const config = {
+    headers: {
+      'Accept': accept,
+      "Content-Type": 'application/x-www-form-urlencoded',
+    }
+  }
+  axios.post(url, body, config)
+    .then(response => res.send(response.data))
+    .catch(error => {
+      try { res.status(error.response.status).send(error.response.data) }
+      catch {
+        try { res.status(400).send(error) }
+        catch { res.status(500).send("unknown error :(") }
+      }
+    });
+});
+
+
+
+
+// query (GET) (params in queryParams)
+// router.get('/:repo', function (req, res) {
+//   const repo = req.params.repo
+//   const params = req.query
+//   // const accept = "text/csv" // HARDCODE text/csv (rdf4j default)
+//   // const accept = "application/json" // HARDCODE application/json
+//   var accept = req.headers['accept'] // use req accept value
+//   if (accept === "*/*") accept = 'application/json' // if no accept is given use JSON
+//   const url = rdf4jServer + 'repositories/' + repo
+//   const config = {
+//     params: params,
+//     headers: {
+//       'Accept': accept,
+//     }
+//   }
+//   axios.get(url, config)
+//     .then(response => {
+//       if (accept === "application/json")
+//         res.jsonp(response.data)
+//       else
+//         res.send(response.data)
+//     })
+//     .catch(error => {
+//       try { res.status(error.response.status).send(error.response.data) }
+//       catch {
+//         try { res.status(400).send(error) }
+//         catch { res.status(500).send("unknown error :(") }
+//       }
+//     });
+// });
+
+// query (POST) (params in x-www-form-urlencoded)
+// router.post('/:repo', function (req, res) {
+//   const repo = req.params.repo
+//   // const accept = "text/csv" // HARDCODE text/csv (rdf4j default)
+//   // const accept = "application/json" // HARDCODE application/json
+//   var accept = req.headers['accept'] // use req accept value
+//   if (accept === "*/*") accept = 'application/json' // if no accept is given use JSON
+//   const url = rdf4jServer + 'repositories/' + repo
+//   const body = qs.stringify(req.body)
+//   const config = {
+//     headers: {
+//       'Accept': accept,
+//       "Content-Type": 'application/x-www-form-urlencoded',
+//     }
+//   }
+//   axios.post(url, body, config)
+//     .then(response => {
+//       if(accept==="application/json")
+//         res.jsonp(response.data)
+//       else
+//         res.send(response.data)
+//     })
+//     .catch(error => {
+//       try { res.status(error.response.status).send(error.response.data) }
+//       catch {
+//         try { res.status(400).send(error) }
+//         catch { res.status(500).send("unknown error :(") }
+//       }
+//     });
+// });
+
+
+
+//// Accept types ////
+//// Table (SELECT)
+// Accept:application/sparql-results+xml
+// Accept:application/sparql-results+json
+// Accept:application/x-binary-rdf-results-table
+// Accept:application/json
+// Accept:application/xml
+// Accept:text/csv
+//// RDF/Triples (CONSTRUCT)
+// Accept:application/rdf+json
+// Accept:application/ld+json
+// Accept:text/plain
+// Accept:text/turtle
+// Accept:application/rdf+xml
+// Accept:text/rdf+n3
+// Accept:text/x-nquads
+// Accept:application/trix
+// Accept:application/x-trig
+// Accept:application/x-binary-rdf
+//// Boolean (ASK)
+// Accept:text/boolean
+// Accept:application/sparql-results+xml
+// Accept:application/sparql-results+json
 
 module.exports = router;
