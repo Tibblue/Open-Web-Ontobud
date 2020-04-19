5539b4306b8ec8f035cbb79374fcf9f51927a2b7
KIKO <fjmo_1997@hotmail.com>
2020-04-19 22:40:54 +0100

    improved feedback

--- backend/models/feedback.js
+++ backend/models/feedback.js
@@ -4,6 +4,10 @@ var Schema = mongoose.Schema
 var FeedbackSchema = new Schema({
   rating: {type: Number, required: true},
   message: {type: String},
+  intuitive: {type: Number, required: true},
+  style: {type: Number, required: true},
+  useAgain: {type: Number, required: true},
+  anyBugs: {type: Number, required: true},
   date: {type: Date, default: Date.now, required: true},
 })
 
--- frontend/src/components/feedback.vue
+++ frontend/src/components/feedback.vue
@@ -6,13 +6,13 @@
         rows="6"
         row-height="22"
         label="Feedback"
-        placeholder="Feedback message"
+        placeholder="Ideas, Bugs, etc"
       ></v-textarea>
     </v-col>
-    <v-col cols="12" class="px-3">
+    <v-col cols="12" class="px-3 py-3">
       <v-slider hide-details
         v-model="rating"
-        label="Rate 0-10⭐"
+        label="Overall experience (0-10⭐)"
         min="0"
         max="10"
         thumb-label="always"
@@ -25,8 +25,56 @@
         </template> -->
       </v-slider>
     </v-col>
+    <v-col cols="12" md="6" class="px-3 py-3">
+      <v-slider hide-details
+        v-model="intuitive"
+        :label="`Intuitive interface ${emojisSatisfaction5[intuitive-1]}`"
+        min="1"
+        max="5"
+      >
+        <template v-slot:thumb-label="{ value }">
+          {{ value=emojisSatisfaction5[intuitive-1] }}
+        </template>
+      </v-slider>
+    </v-col>
+    <v-col cols="12" md="6" class="px-3 py-3">
+      <v-slider hide-details
+        v-model="style"
+        :label="`Interface design ${emojisSatisfaction5[style-1]}`"
+        min="1"
+        max="5"
+      >
+        <template v-slot:thumb-label="{ value }">
+          {{ value=emojisSatisfaction5[style-1] }}
+        </template>
+      </v-slider>
+    </v-col>
+    <v-col cols="12" md="6" class="px-3 py-3">
+      <v-slider hide-details
+        v-model="useAgain"
+        :label="`Would use again? ${emojisYesNo[useAgain+1]}`"
+        min="-1"
+        max="1"
+      >
+        <template v-slot:thumb-label="{ value }">
+          {{ value=emojisYesNo[useAgain+1] }}
+        </template>
+      </v-slider>
+    </v-col>
+    <v-col cols="12" md="6" class="px-3 py-3">
+      <v-slider hide-details
+        v-model="anyBugs"
+        :label="`Any Bugs? ${emojisYesNo[anyBugs+1]}`"
+        min="-1"
+        max="1"
+      >
+        <template v-slot:thumb-label="{ value }">
+          {{ value=emojisYesNo[anyBugs+1] }}
+        </template>
+      </v-slider>
+    </v-col>
     <v-col cols="12">
-      <v-btn :loading="loading.feedbackSubmit" block color="primary" @click="feedback(message, rating)">
+      <v-btn :loading="loading.feedbackSubmit" block color="primary" @click="feedback()">
         Submit Feedback
       </v-btn>
       <v-alert text dismissible type="success" v-model="alert.feedbackSubmitSuccess">
@@ -46,8 +94,14 @@ const qs = require('querystring')
 
 export default {
   data: () => ({
-    // satisfactionEmojis: ['😭', '😢', '☹️', '🙁', '😐', '🙂', '😊', '😁', '😄', '😍'],
+    // emojisSatisfaction10: ['😭', '😢', '☹️', '🙁', '😐', '🙂', '😊', '😁', '😄', '😍'],
+    emojisSatisfaction5: ['😭', '☹️', '😐', '😁', '😍'],
+    emojisYesNo: ['👎', '🤔', '👍'],
     rating: 5,
+    intuitive: 3,
+    style: 3,
+    useAgain: 0,
+    anyBugs: 0,
     message: '',
     loading: {
       feedbackSubmit: false
@@ -71,11 +125,15 @@ export default {
     }
   },
   methods: {
-    feedback (message, rating) {
+    feedback () {
       this.loading.feedbackSubmit = true
       var form = {}
-      form.message = message
-      form.rating = rating
+      form.message = this.message
+      form.rating = this.rating
+      form.intuitive = this.intuitive
+      form.style = this.style
+      form.useAgain = this.useAgain
+      form.anyBugs = this.anyBugs
       axios.post(this.backendURL + '/api/feedback', qs.stringify(form),
         { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
       )
